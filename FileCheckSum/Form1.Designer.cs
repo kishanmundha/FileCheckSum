c363537b2b68223f05ab2f4e1b69a4d0eaa6f579
kishan.mundha <kishan.mundha@live.com>
2015-07-15 07:53:33 +0530

    commit

---
+++ FileCheckSum/Form1.Designer.cs
@@ -0,0 +1,180 @@
+﻿namespace FileCheckSum
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBoxFolder = new System.Windows.Forms.TextBox();
+            this.buttonBrowseFolder = new System.Windows.Forms.Button();
+            this.buttonOK = new System.Windows.Forms.Button();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.labelMundhaSoft = new System.Windows.Forms.LinkLabel();
+            this.labelStatus = new System.Windows.Forms.Label();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // textBoxFolder
+            // 
+            this.textBoxFolder.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.textBoxFolder.Location = new System.Drawing.Point(12, 23);
+            this.textBoxFolder.Name = "textBoxFolder";
+            this.textBoxFolder.Size = new System.Drawing.Size(486, 22);
+            this.textBoxFolder.TabIndex = 0;
+            // 
+            // buttonBrowseFolder
+            // 
+            this.buttonBrowseFolder.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonBrowseFolder.Location = new System.Drawing.Point(504, 23);
+            this.buttonBrowseFolder.Name = "buttonBrowseFolder";
+            this.buttonBrowseFolder.Size = new System.Drawing.Size(30, 23);
+            this.buttonBrowseFolder.TabIndex = 1;
+            this.buttonBrowseFolder.Text = "...";
+            this.buttonBrowseFolder.UseVisualStyleBackColor = true;
+            this.buttonBrowseFolder.Click += new System.EventHandler(this.buttonBrowseFolder_Click);
+            // 
+            // buttonOK
+            // 
+            this.buttonOK.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.buttonOK.Location = new System.Drawing.Point(454, 315);
+            this.buttonOK.Name = "buttonOK";
+            this.buttonOK.Size = new System.Drawing.Size(80, 23);
+            this.buttonOK.TabIndex = 2;
+            this.buttonOK.Text = "Run";
+            this.buttonOK.UseVisualStyleBackColor = true;
+            this.buttonOK.Click += new System.EventHandler(this.buttonOK_Click);
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar1.Location = new System.Drawing.Point(12, 283);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(522, 23);
+            this.progressBar1.TabIndex = 3;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(328, 321);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(84, 17);
+            this.checkBox1.TabIndex = 5;
+            this.checkBox1.Text = "Check Only";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // labelMundhaSoft
+            // 
+            this.labelMundhaSoft.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelMundhaSoft.AutoSize = true;
+            this.labelMundhaSoft.Location = new System.Drawing.Point(12, 325);
+            this.labelMundhaSoft.Name = "labelMundhaSoft";
+            this.labelMundhaSoft.Size = new System.Drawing.Size(72, 13);
+            this.labelMundhaSoft.TabIndex = 6;
+            this.labelMundhaSoft.TabStop = true;
+            this.labelMundhaSoft.Text = "MundhaSoft";
+            this.labelMundhaSoft.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.labelMundhaSoft_LinkClicked);
+            // 
+            // labelStatus
+            // 
+            this.labelStatus.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.labelStatus.Location = new System.Drawing.Point(12, 240);
+            this.labelStatus.Name = "labelStatus";
+            this.labelStatus.Size = new System.Drawing.Size(522, 34);
+            this.labelStatus.TabIndex = 7;
+            // 
+            // listView1
+            // 
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 63);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(520, 165);
+            this.listView1.TabIndex = 8;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            this.listView1.SizeChanged += new System.EventHandler(this.listView1_SizeChanged);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "Message";
+            this.columnHeader1.Width = 150;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Path";
+            this.columnHeader2.Width = 360;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(546, 350);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.labelStatus);
+            this.Controls.Add(this.labelMundhaSoft);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.buttonOK);
+            this.Controls.Add(this.buttonBrowseFolder);
+            this.Controls.Add(this.textBoxFolder);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "MD5 File Checksum 1.1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBoxFolder;
+        private System.Windows.Forms.Button buttonBrowseFolder;
+        private System.Windows.Forms.Button buttonOK;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.LinkLabel labelMundhaSoft;
+        private System.Windows.Forms.Label labelStatus;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+    }
+}
+
---
+++ FileCheckSum/Form1.cs
@@ -0,0 +1,382 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FileCheckSum
+{
+    public partial class Form1 : Form
+    {
+        #region Variables
+        private Timer timer;
+
+        private long TotalBytes = 0;
+        private long BytePassed = 0;
+
+        #endregion
+
+        #region Main Function
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+        }
+
+        #endregion
+
+        #region Events
+        private void buttonBrowseFolder_Click(object sender, EventArgs e)
+        {
+            using (FolderBrowserDialog fbd = new FolderBrowserDialog())
+            {
+                fbd.ShowNewFolderButton = false;
+                fbd.SelectedPath = textBoxFolder.Text;
+                if (fbd.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    textBoxFolder.Text = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void buttonOK_Click(object sender, EventArgs e)
+        {
+            BackgroundWorker bg = new BackgroundWorker();
+            bg.WorkerReportsProgress = true;
+            bg.DoWork += bg_DoWork;
+            bg.RunWorkerCompleted += bg_RunWorkerCompleted;
+            bg.ProgressChanged += bg_ProgressChanged;
+
+            timer = new Timer();
+            timer.Tick += timer_Tick;
+            timer.Interval = 1000;
+
+            buttonOK.Enabled = buttonBrowseFolder.Enabled = textBoxFolder.Enabled = checkBox1.Enabled = false;
+            progressBar1.Visible = true;
+
+            // clear message
+            listView1.Items.Clear();
+            showlabelmsg("");
+
+            bg.RunWorkerAsync();
+            timer.Start();
+        }
+
+        private void timer_Tick(object sender, EventArgs e)
+        {
+            if (TotalBytes == 0)
+            {
+                progressBar1.Style = ProgressBarStyle.Marquee;
+            }
+            else
+            {
+                progressBar1.Style = ProgressBarStyle.Blocks;
+                progressBar1.Value = (int)Math.Max(Math.Min(100, (BytePassed * 100) / TotalBytes), 0);
+            }
+        }
+
+        private void bg_DoWork(object sender, DoWorkEventArgs e)
+        {
+            // Clear old value (May be take time to get new value)
+            TotalBytes = 0;
+            BytePassed = 0;
+
+            showlabelmsg(string.Format("Calculating total size..."));
+
+            TotalBytes = GetDirectorySize(textBoxFolder.Text);
+
+            ScanDirectory(textBoxFolder.Text);
+        }
+
+        private void bg_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            buttonOK.Enabled = buttonBrowseFolder.Enabled = textBoxFolder.Enabled = checkBox1.Enabled = true;
+            progressBar1.Value = 100;
+            showlabelmsg("");
+
+            BackgroundWorker bg = sender as BackgroundWorker;
+            bg.Dispose();
+
+            timer.Stop();
+            timer.Dispose();
+        }
+
+        private void bg_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            progressBar1.Value = Math.Max(0, Math.Min(100, e.ProgressPercentage));
+        }
+
+        private void labelMundhaSoft_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            // Show my projects
+            System.Diagnostics.Process.Start("https://www.facebook.com/mundhaSoft");
+        }
+
+        private void listView1_SizeChanged(object sender, EventArgs e)
+        {
+            listView1.Columns[1].Width = listView1.Width - 160;
+        }
+        #endregion
+
+        #region Functions
+        /// <summary>
+        /// Get total size of directory
+        /// </summary>
+        /// <param name="dir">Directory Path</param>
+        /// <returns>Size in bytes</returns>
+        private long GetDirectorySize(string dir)
+        {
+            try
+            {
+                long size = 0;
+
+                // Make sure that dir ended with backslash
+                if (!dir.EndsWith("\\"))
+                    dir += "\\";
+
+                // Make sure that directory exists in system
+                if (!Directory.Exists(dir))
+                    throw new DirectoryNotFoundException(string.Format("{0} not found", dir));
+
+                // Get list of directories and files
+                string[] directories = Directory.GetDirectories(dir);
+                string[] files = Directory.GetFiles(dir);
+
+                foreach (string fileName in files)
+                {
+                    FileInfo fInfo = new FileInfo(fileName);
+                    size += fInfo.Length;
+                }
+
+                foreach (string dirName in directories)
+                {
+                    size += GetDirectorySize(dirName);
+                }
+
+                return size;
+            }
+            catch
+            {
+                return 0;
+            }
+        }
+
+        private void ScanDirectory(string dir)
+        {
+            try
+            {
+                // show status dir
+                showlabelmsg(dir);
+
+                // Make sure that dir ended with backslash
+                if (!dir.EndsWith("\\"))
+                    dir += "\\";
+
+                // Make sure that directory exists in system
+                if (!Directory.Exists(dir))
+                    throw new DirectoryNotFoundException(string.Format("{0} not found", dir));
+
+                // Get list of directories and files
+                string[] directories = Directory.GetDirectories(dir);
+                string[] files = Directory.GetFiles(dir);
+
+                // No directory or file found
+                if (directories.Length + files.Length == 0)
+                    return;
+
+                #region File checksum
+                // Local variable for old md5 checksum and new md5 checksum
+                Dictionary<string, string> md5old = new Dictionary<string, string>();
+                Dictionary<string, string> md5new = new Dictionary<string, string>();
+
+                // Read old md5 checksum
+                // and store in local variable
+                if (File.Exists(dir + "checksum.md5"))
+                {
+                    foreach (string checksum_old in File.ReadAllLines(dir + "checksum.md5"))
+                    {
+                        string[] s = checksum_old.Split('\t');
+                        if (s.Length == 2)
+                            md5old.Add(s[0], s[1]);
+                    }
+
+                }
+
+                foreach (String fileName in files)
+                {
+                    FileInfo fInfo = new FileInfo(fileName);
+
+                    // dont make checksum of old checksum info file
+                    if (fInfo.Extension == ".md5")
+                    {
+                        continue;
+                    }
+
+                    showlabelmsg(string.Format("{0} (size: {1})", fileName, getFileSizeString(fInfo.Length)));
+
+                    string md5hash = md5File(fileName);
+                    md5new.Add(fInfo.Name, md5hash);
+
+                    // update progress
+                    BytePassed += fInfo.Length;
+                }
+
+                // only update checksum file if need
+                bool IsUpdateChecksumFile = false;
+
+                // old file and new file count not match
+                // so files are added or deleted
+                // must be update checksum
+                if (md5old.Count != md5new.Count)
+                    IsUpdateChecksumFile = true;
+
+                foreach (KeyValuePair<string, string> md5Value in md5new)
+                {
+                    // old md5 checksum not found
+                    // may be new file created
+                    if (!md5old.ContainsKey(md5Value.Key))
+                    {
+                        addListItem("New file", dir + md5Value.Key);
+                        IsUpdateChecksumFile = true;
+                        continue;
+                    }
+
+                    // old md5 checksum and new md5 checksum not match
+                    if (md5old[md5Value.Key] != md5Value.Value)
+                    {
+                        addListItem("Missmatch", dir + md5Value.Key);
+                        IsUpdateChecksumFile = true;
+                        continue;
+                    }
+                }
+
+                // update checksum file if required
+                if (!checkBox1.Checked && IsUpdateChecksumFile)
+                {
+                    StreamWriter md5checksumFile = File.CreateText(dir + "checksum.md5");
+                    foreach (KeyValuePair<string, string> md5Value in md5new)
+                    {
+                        md5checksumFile.WriteLine(string.Format("{0}\t{1}", md5Value.Key, md5Value.Value));
+                    }
+                    md5checksumFile.Close();
+                }
+
+                #endregion
+
+                // Scan Sub Direcotries
+                foreach (string directoryName in directories)
+                {
+                    ScanDirectory(directoryName);
+                }
+            }
+            catch (Exception ex)
+            {
+                addListItem("Error", ex.Message, Color.Red);
+            }
+        }
+
+        /// <summary>
+        /// Get MD5 value of file
+        /// </summary>
+        /// <param name="fileName">File Path</param>
+        /// <returns></returns>
+        private string md5File(string fileName)
+        {
+            if (!File.Exists(fileName))
+                throw new FileNotFoundException(string.Format("{0} not found", fileName));
+
+            FileStream fs = File.OpenRead(fileName);
+
+            MD5 md5Hash = MD5.Create();
+            byte[] data = md5Hash.ComputeHash(fs);
+
+            fs.Close();
+
+            // Create a new Stringbuilder to collect the bytes 
+            // and create a string.
+            StringBuilder sBuilder = new StringBuilder();
+
+            // Loop through each byte of the hashed data  
+            // and format each one as a hexadecimal string. 
+            for (int i = 0; i < data.Length; i++)
+            {
+                sBuilder.Append(data[i].ToString("x2"));
+            }
+
+            return sBuilder.ToString();
+        }
+
+        /// <summary>
+        /// Convert long to KB,MB
+        /// </summary>
+        /// <param name="size">size in Bytes</param>
+        /// <returns>String format of size</returns>
+        private string getFileSizeString(long size)
+        {
+            string[] units = new string[] { "Bytes", "KB", "MB", "GB", "TB" };
+
+            int pos = 0;
+
+            //int intSize = (int)size;
+
+            while (size > 1024 && pos < units.Length)
+            {
+                pos++;
+                size /= 1024;
+            }
+
+            return string.Format("{0} {1}", (int)size, units[pos]);
+        }
+
+        private void addListItem(string msg, string path)
+        {
+            addListItem(msg, path, Color.Black);
+        }
+
+        private void addListItem(string msg, string path, Color color)
+        {
+            ListViewItem liv = new ListViewItem(msg);
+            liv.SubItems.Add(path);
+            liv.ForeColor = color;
+
+            if (!listView1.InvokeRequired)
+            {
+                listView1.Items.Add(liv);
+            }
+            else
+            {
+                listView1.Invoke(new MethodInvoker(delegate()
+                {
+                    listView1.Items.Add(liv);
+                }));
+            }
+        }
+
+        // Show current message
+        private void showlabelmsg(string message)
+        {
+            if (!labelStatus.InvokeRequired)
+            {
+                labelStatus.Text = message;
+            }
+            else
+            {
+                labelStatus.Invoke(new MethodInvoker(delegate()
+                {
+                    labelStatus.Text = message;
+                }));
+            }
+        }
+
+        #endregion
+    }
+}
